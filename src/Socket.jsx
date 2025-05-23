f5e3da50e38f7836a0d7aad5234a9077bf0e14a2
Mohammed Afnan <afnankk9995@gmail.com>
2025-05-23 11:08:51 +0530

    message error fix

--- src/Socket.jsx
+++ src/Socket.jsx
@@ -1,7 +1,7 @@
 
 import { io } from 'socket.io-client';
-const baseUrl = import.meta.env.VITE_BASE_URL;
+// const baseUrl = import.meta.env.VITE_BASE_URL;
 
-const socket = io(baseUrl);
+const socket = io("https://skillswap-backend-65xf.onrender.com");
 
 export default socket;
