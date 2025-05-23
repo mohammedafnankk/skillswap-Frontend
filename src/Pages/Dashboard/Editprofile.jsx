8c3c637915dc209b1c5772a989ee619dd887531f
Mohammed Afnan <afnankk9995@gmail.com>
2025-05-23 22:29:19 +0530

    avatar loading set

--- src/Pages/Dashboard/Editprofile.jsx
+++ src/Pages/Dashboard/Editprofile.jsx
@@ -270,8 +270,12 @@ function Editprofile() {
                 </p>
               </div>
               <div className="p-6 pt-0 max-sm:p-5">
-                <div className="flex gap-6 max-sm:flex-col max-sm:items-center">
+                <div className="flex gap-6 max-sm:flex-col max-sm:items-center relative">
                   <span>
+                    {isLoading?
+                    
+                    <span className="absolute top-10 left-[4.8%] h-9 w-9 border-black border-2 border-t-transparent rounded-full animate-spin"></span>
+                    :""}
                     <img
                       src={avatar.avatar ? avatar.avatar : profileImg}
                       alt=""
